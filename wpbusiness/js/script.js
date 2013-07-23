4d12c7dd774031d0119133c44adf82b4acf2e7f9
ZverushkaX <svchost@inbox.ru>
2013-07-23 19:13:22 +0300

    ready page

--- wpbusiness/js/script.js
+++ wpbusiness/js/script.js
@@ -1,3 +1,25 @@
 // Загружена ли библиотека
 window.jQuery || document.write('<script type="text/javascript" src="js/jquery-1.9.1.min.js"><\/script>');
 
+$(document).ready(function(){
+	$("#email-name").focus(function(){
+		if ($(this).val()=="Name") {				
+			$(this).val('');
+		}
+	});
+	$("#email-name").blur(function(){
+		if ($(this).val()=="") {
+			$(this).val("Name");
+		}
+	});
+	$("#email-adress").focus(function(){
+		if ($(this).val()=="Email Address") {
+			$(this).val('');
+		}
+	});
+	$("#email-adress").blur(function(){
+		if ($(this).val()=="") {
+			$(this).val("Email Address");
+		}
+	});
+});
