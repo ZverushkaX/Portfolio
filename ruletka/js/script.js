2f955d029e451b379cd959d7b03818bfa955b995
ZverushkaX <svchost@inbox.ru>
2013-07-29 00:24:54 +0300

    Куча работы!!! :(

--- ruletka/js/script.js
+++ ruletka/js/script.js
@@ -1,3 +1,10 @@
 // Загружена ли библиотека
 window.jQuery || document.write('<script type="text/javascript" src="js/jquery-1.9.1.min.js"><\/script>');
 
+$(function() {
+    if (window.PIE) {
+         // $('.header').children.each(function() {
+         //     PIE.attach(this);
+         // });
+    }
+});
